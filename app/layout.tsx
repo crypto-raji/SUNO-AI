a3d80dc38163012f1e1c854ce2bca9c4badb56d9
reactjay <japhethadamu561@gmail.com>
2026-09-12 18:23:33 +0100

    Add suppressHydrationWarning to prevent React error 418/423

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body className="font-body antialiased">{children}</body>
+      <body className="font-body antialiased" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
